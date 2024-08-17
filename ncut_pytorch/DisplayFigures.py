db3dceecb6cb5ed72ba19a994c39e3d323773190
Eric Xu <eiyxu03@gmail.com>
2024-08-17 17:34:28 -0400

    Implemented ncut_pytorch and obtained DINOv2 feature layers

---
+++ ncut_pytorch/DisplayFigures.py
@@ -0,0 +1,6 @@
+### ABOUT THIS PROGRAM: Helps display each of the DINOv2 features for each image in a more appealing way
+
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
