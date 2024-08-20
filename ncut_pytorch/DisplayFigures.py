1673a50b9ebaaaa2ecaf576a6825c96000bbe024
Eric Xu <eiyxu03@gmail.com>
2024-08-20 13:02:34 -0400

    Used ncut_pytorch to assess DINO features between different images

--- ncut_pytorch/DisplayFigures.py
+++ ncut_pytorch/DisplayFigures.py
@@ -3,4 +3,206 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+
+### HELPER CODE AND FUNCTIONS:
+
+# Valid poses: ["male_thin", "male_wide", "female_thin", "female_wide"]
+
+def display(image_number: int, target_pose: str, layer_number: int): # only top and upper poses available
+
+    # First creates the figure
+    fig = plt.figure(figsize=(30, 21)) # maybe enlarge
+
+    # 2. Sets the values of rows and column variables 
+    rows = 5
+    columns = 5
+
+    # 3. Reading images 
+
+    # Originals
+    selfie_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/selfie_faces/image" + str(image_number) + "_selfie_face.png").convert('RGB')
+    unihuman_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/unihuman_faces/image" + str(image_number) + "_" + str(target_pose) + "_unihuman_face.png").convert('RGB')
+    swapped_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/swapped_faces/image" + str(image_number) + "_" + str(target_pose) + "_swapped_face.png").convert('RGB')
+    target_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/target_faces/" + str(target_pose) + "_face.png").convert('RGB') 
+    texture_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/texture_map_faces/image" + str(image_number) + "_" + str(target_pose) + "_texture_map_face.png").convert('RGB')
+
+    # UMAP 100 Eigenvectors
+    umap_100_selfie_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfie_faces/Selfie_Face" + str(image_number) + "_Layer" + str(layer_number) + "_100Eigenvectors_UMAP_DINOv2.jpg").convert('RGB') 
+    umap_100_unihuman_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_unihuman_faces/UniHuman_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_100Eigenvectors_UMAP_DINOv2.jpg").convert('RGB') 
+    umap_100_swapped_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_swapped_faces/Swapped_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_100Eigenvectors_UMAP_DINOv2.jpg").convert('RGB') 
+    umap_100_target_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_target_faces/" + str(target_pose) + "_top_Layer" + str(layer_number) + "_100Eigenvectors_UMAP_DINOv2.jpg").convert('RGB')
+    umap_100_texture_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_texture_faces/Texture_Face" + str(image_number) + "_" + str(target_pose) + "_Layer" + str(layer_number) + "_100Eigenvectors_UMAP_DINOv2.jpg").convert('RGB')
+
+    # UMAP 200 Eigenvectors
+    umap_200_selfie_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfie_faces/Selfie_Face" + str(image_number) + "_Layer" + str(layer_number) + "_200Eigenvectors_UMAP_DINOv2.jpg").convert('RGB') 
+    umap_200_unihuman_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_unihuman_faces/UniHuman_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_200Eigenvectors_UMAP_DINOv2.jpg").convert('RGB') 
+    umap_200_swapped_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_swapped_faces/Swapped_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_200Eigenvectors_UMAP_DINOv2.jpg").convert('RGB') 
+    umap_200_target_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_target_faces/" + str(target_pose) + "_top_Layer" + str(layer_number) + "_200Eigenvectors_UMAP_DINOv2.jpg").convert('RGB')
+    umap_200_texture_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_texture_faces/Texture_Face" + str(image_number) + "_" + str(target_pose) + "_Layer" + str(layer_number) + "_200Eigenvectors_UMAP_DINOv2.jpg").convert('RGB')
+
+
+    # tSNE 100 Eigenvectors
+    tSNE_100_selfie_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfie_faces/Selfie_Face" + str(image_number) + "_Layer" + str(layer_number) + "_100Eigenvectors_tSNE_DINOv2.jpg").convert('RGB') 
+    tSNE_100_unihuman_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_unihuman_faces/UniHuman_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_100Eigenvectors_tSNE_DINOv2.jpg").convert('RGB') 
+    tSNE_100_swapped_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_swapped_faces/Swapped_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_100Eigenvectors_tSNE_DINOv2.jpg").convert('RGB') 
+    tSNE_100_target_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_target_faces/" + str(target_pose) + "_top_Layer" + str(layer_number) + "_100Eigenvectors_tSNE_DINOv2.jpg").convert('RGB')
+    tSNE_100_texture_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_texture_faces/Texture_Face" + str(image_number) + "_" + str(target_pose) + "_Layer" + str(layer_number) + "_100Eigenvectors_tSNE_DINOv2.jpg").convert('RGB')
+
+
+    # tSNE 200 Eigenvectors
+    tSNE_200_selfie_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfie_faces/Selfie_Face" + str(image_number) + "_Layer" + str(layer_number) + "_200Eigenvectors_tSNE_DINOv2.jpg").convert('RGB') 
+    tSNE_200_unihuman_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_unihuman_faces/UniHuman_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_200Eigenvectors_tSNE_DINOv2.jpg").convert('RGB') 
+    tSNE_200_swapped_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_swapped_faces/Swapped_Face" + str(image_number) + "_" + str(target_pose) +  "_Layer" + str(layer_number) + "_200Eigenvectors_tSNE_DINOv2.jpg").convert('RGB') 
+    tSNE_200_target_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_target_faces/" + str(target_pose) + "_top_Layer" + str(layer_number) + "_200Eigenvectors_tSNE_DINOv2.jpg").convert('RGB')
+    tSNE_200_texture_face = Image.open("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_texture_faces/Texture_Face" + str(image_number) + "_" + str(target_pose) + "_Layer" + str(layer_number) + "_200Eigenvectors_tSNE_DINOv2.jpg").convert('RGB')
+
+    # 4. Adds original subplots at corresponding positions -----
+    fig.add_subplot(rows, columns, 1) 
+    plt.imshow(selfie_face) # Showing selfie_face image 
+    plt.axis('off') 
+    plt.title("original selfie_face")
+
+    fig.add_subplot(rows, columns, 2) 
+    plt.imshow(unihuman_face) # Showing unihuman_face image 
+    plt.axis('off') 
+    plt.title("original unihuman_face")
+
+    fig.add_subplot(rows, columns, 3) 
+    plt.imshow(swapped_face) # Showing swapped_face image 
+    plt.axis('off') 
+    plt.title("original swapped_face")
+
+    fig.add_subplot(rows, columns, 4) 
+    plt.imshow(target_face) # Showing target_face image 
+    plt.axis('off') 
+    plt.title("original target_face")
+
+    fig.add_subplot(rows, columns, 5) 
+    plt.imshow(texture_face) # Showing texture_face image 
+    plt.axis('off') 
+    plt.title("original texture_face")
+
+    # 5. Adds the UMAP 100 Eigenvector subplots at corresponding positions -----
+    fig.add_subplot(rows, columns, 6) 
+    plt.imshow(umap_100_selfie_face) # Showing selfie_face image 
+    plt.axis('off') 
+    plt.title("umap 100 selfie_face")
+
+    fig.add_subplot(rows, columns, 7) 
+    plt.imshow(umap_100_unihuman_face) # Showing unihuman_face image 
+    plt.axis('off') 
+    plt.title("umap 100 unihuman_face")
+
+    fig.add_subplot(rows, columns, 8) 
+    plt.imshow(umap_100_swapped_face) # Showing swapped_face image 
+    plt.axis('off') 
+    plt.title("umap 100 swapped_face")
+
+    fig.add_subplot(rows, columns, 9) 
+    plt.imshow(umap_100_target_face) # Showing target_face image 
+    plt.axis('off') 
+    plt.title("umap 100 target_face")
+
+    fig.add_subplot(rows, columns, 10) 
+    plt.imshow(umap_100_texture_face) # Showing texture_face image 
+    plt.axis('off') 
+    plt.title("umap 100 texture_face")
+
+    # 6. Adds the UMAP 200 Eigenvector subplots at corresponding positions -----
+    fig.add_subplot(rows, columns, 11) 
+    plt.imshow(umap_200_selfie_face) # Showing selfie_face image 
+    plt.axis('off') 
+    plt.title("umap 200 selfie_face")
+
+    fig.add_subplot(rows, columns, 12) 
+    plt.imshow(umap_200_unihuman_face) # Showing unihuman_face image 
+    plt.axis('off') 
+    plt.title("umap 200 unihuman_face")
+
+    fig.add_subplot(rows, columns, 13) 
+    plt.imshow(umap_200_swapped_face) # Showing swapped_face image 
+    plt.axis('off') 
+    plt.title("umap 200 swapped_face")
+
+    fig.add_subplot(rows, columns, 14) 
+    plt.imshow(umap_200_target_face) # Showing target_face image 
+    plt.axis('off') 
+    plt.title("umap 200 target_face")
+
+    fig.add_subplot(rows, columns, 15) 
+    plt.imshow(umap_200_texture_face) # Showing texture_face image 
+    plt.axis('off') 
+    plt.title("umap 200 texture_face")
+
+    # 7. Adds the tSNE 100 Eigenvector subplots at corresponding positions -----
+    fig.add_subplot(rows, columns, 16) 
+    plt.imshow(tSNE_100_selfie_face) # Showing selfie_face image 
+    plt.axis('off') 
+    plt.title("tSNE 100 selfie_face")
+
+    fig.add_subplot(rows, columns, 17) 
+    plt.imshow(tSNE_100_unihuman_face) # Showing unihuman_face image 
+    plt.axis('off') 
+    plt.title("tSNE 100 unihuman_face")
+
+    fig.add_subplot(rows, columns, 18) 
+    plt.imshow(tSNE_100_swapped_face) # Showing swapped_face image 
+    plt.axis('off') 
+    plt.title("tSNE 100 swapped_face")
+
+    fig.add_subplot(rows, columns, 19) 
+    plt.imshow(tSNE_100_target_face) # Showing target_face image 
+    plt.axis('off') 
+    plt.title("tSNE 100 target_face")
+
+    fig.add_subplot(rows, columns, 20) 
+    plt.imshow(tSNE_100_texture_face) # Showing texture_face image 
+    plt.axis('off') 
+    plt.title("tSNE 100 texture_face")
+
+    # 6. Adds the tSNE 200 Eigenvector subplots at corresponding positions -----
+    fig.add_subplot(rows, columns, 21) 
+    plt.imshow(tSNE_200_selfie_face) # Showing selfie_face image 
+    plt.axis('off') 
+    plt.title("tSNE 200 selfie_face")
+
+    fig.add_subplot(rows, columns, 22) 
+    plt.imshow(tSNE_200_unihuman_face) # Showing unihuman_face image 
+    plt.axis('off') 
+    plt.title("tSNE 200 unihuman_face")
+
+    fig.add_subplot(rows, columns, 23) 
+    plt.imshow(tSNE_200_swapped_face) # Showing swapped_face image 
+    plt.axis('off') 
+    plt.title("tSNE 200 swapped_face")
+
+    fig.add_subplot(rows, columns, 24) 
+    plt.imshow(tSNE_200_target_face) # Showing target_face image 
+    plt.axis('off') 
+    plt.title("tSNE 200 target_face")
+
+    fig.add_subplot(rows, columns, 25) 
+    plt.imshow(tSNE_200_texture_face) # Showing texture_face image 
+    plt.axis('off') 
+    plt.title("tSNE 200 texture_face")
+
+    plt.suptitle("Image" + str(image_number) + "_" + str(target_pose) + "_Layer" + str(layer_number))
+
+    # 10. Saves the Matplot figure!
+    plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/compilations/Image" + str(image_number) + "_" + str(target_pose) + "_Layer" + str(layer_number) + "_Compilation.jpg")
+
+### AUTOMATED CODE
+
+images = [14, 23, 39]
+poses = ["male_thin", "male_wide", "female_thin", "female_wide"]
+
+for image_number in images:
+    for pose in poses:
+        for layer_number in range(0, 12):
+            display(image_number, pose, layer_number)
+
+
+
+
 
--- ncut_pytorch/NCUT_Feature_Extraction.py
+++ ncut_pytorch/NCUT_Feature_Extraction.py
@@ -13,142 +13,125 @@ from mpl_toolkits.mplot3d import Axes3D
 from ncut_pytorch import rgb_from_umap_sphere
 from ncut_pytorch import rgb_from_tsne_3d
 
-# Let's use process on images [5, 14, 23, 39, 63, 70, 72, 93]
+# Let's use process on images [14, 23, 39, 63, 70, 72, 93]
 
-image_number = 14
-number_eigenvectors = 100
+images = [14, 23, 39]
+eigenvector_counts = [100, 200]
+poses = ["male_thin", "male_wide", "female_thin", "female_wide"]
 
-for layer_number in range(0, 12):
-    UMAP = True
-    tSNE = False
+for image_number in images:
+    for pose in poses:
+        for layer_number in range(0, 12):
+            for number_eigenvectors in eigenvector_counts:
+                UMAP = True
+                tSNE = False
 
-    # 1. Load the input image
-    url = "/mnt/volume2/Data/UnselfieData/selfies/image" + str(image_number) + ".jpg"
-    image = Image.open(url)
+                # 1. Load the input image
+                url = "/home/kate/Selfie2Portrait/ncut_pytorch/texture_map_faces/image" + str(image_number) + "_" + str(pose) + "_texture_map_face.png"
+                image = Image.open(url).convert('RGB') # needed just in case there are four channels
 
-    # 2. Use DINOv2 features
-    features = feature_extractor(image, resolution=(448, 448), layer=layer_number) # can change layer number here!!!
-    h, w, c = features.shape
+                # 2. Use DINOv2 features --> maybe try higher resolution? used to be (448, 448) -> must be multiple of patch height 14
+                features = feature_extractor(image, resolution=(700, 700), layer=layer_number) # can change layer number here!!!
+                h, w, c = features.shape
 
-    # 3. Flatten the pixels into nodes
-    n = h * w  # flatten the pixels into nodes
-    features = features.reshape(n, c)
-    model = NCUT(num_eig=number_eigenvectors) # INCREASE THIS NUMBER TO 100 -> 150 -> 200; TINKER HERE * * *
-    eigenvectors, eigenvalues = model.fit_transform(features)
+                # 3. Flatten the pixels into nodes
+                n = h * w  # flatten the pixels into nodes
+                features = features.reshape(n, c)
+                model = NCUT(num_eig=number_eigenvectors) # INCREASE THIS NUMBER TO 100 -> 150 -> 200; TINKER HERE * * *
+                eigenvectors, eigenvalues = model.fit_transform(features)
 
-    # 4. 
-    fig, axs = plt.subplots(3, 4, figsize=(13, 10))
-    i_eig = 0
-    for i_row in range(3):
-        for i_col in range(1, 4):
-            ax = axs[i_row, i_col]
-            ax.imshow(eigenvectors[:, i_eig].reshape(h, w), cmap="coolwarm", vmin=-0.1, vmax=0.1)
-            ax.set_title(f"lambda_{i_eig} = {eigenvalues[i_eig]:.3f}")
-            ax.axis("off")
-            i_eig += 1
-    for i_row in range(3):
-        ax = axs[i_row, 0]
-        start, end = i_row * 3, (i_row + 1) * 3
-        rgb = quantile_normalize(eigenvectors[:, start:end]).reshape(h, w, 3)
-        ax.imshow(rgb)
-        ax.set_title(f"eigenvectors {start}-{end-1}")
-        ax.axis("off")
-    plt.suptitle("Top 9 eigenvectors of Ncut DiNOv2 last layer features")
-    plt.tight_layout()
-    # plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features/TOP9Eigenvectors.jpg") # --> Likely don't need
+                # # 4. Unused figure generation
+                # fig, axs = plt.subplots(3, 4, figsize=(13, 10))
+                # i_eig = 0
+                # for i_row in range(3):
+                #     for i_col in range(1, 4):
+                #         ax = axs[i_row, i_col]
+                #         ax.imshow(eigenvectors[:, i_eig].reshape(h, w), cmap="coolwarm", vmin=-0.1, vmax=0.1)
+                #         ax.set_title(f"lambda_{i_eig} = {eigenvalues[i_eig]:.3f}")
+                #         ax.axis("off")
+                #         i_eig += 1
+                # for i_row in range(3):
+                #     ax = axs[i_row, 0]
+                #     start, end = i_row * 3, (i_row + 1) * 3
+                #     rgb = quantile_normalize(eigenvectors[:, start:end]).reshape(h, w, 3)
+                #     ax.imshow(rgb)
+                #     ax.set_title(f"eigenvectors {start}-{end-1}")
+                #     ax.axis("off")
+                # plt.suptitle("Top 9 eigenvectors of Ncut DiNOv2 last layer features")
+                # plt.tight_layout()
+                # plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features/TOP9Eigenvectors.jpg") # --> Likely don't need
 
-    # 5. 
-    def plot_3d_animation(X_3d, rgb, title):
-        x, y, z = X_3d.T
-        fig = plt.figure(figsize=(10, 5))
+                # 5. 
+                # def plot_3d_animation(X_3d, rgb, title):
+                #     x, y, z = X_3d.T
+                #     fig = plt.figure(figsize=(10, 5))
 
-        # Add a subplot for the static image
-        ax1 = fig.add_subplot(121)
-        ax1.imshow(rgb.reshape(h, w, 3))
-        ax1.axis('off')  # Hide axes
+                #     # Add a subplot for the static image
+                #     ax1 = fig.add_subplot(121)
+                #     ax1.imshow(rgb.reshape(h, w, 3))
+                #     ax1.axis('off')  # Hide axes
 
-        # Add a subplot for the 3D scatter plot
-        ax = fig.add_subplot(122, projection='3d')
-        scat = ax.scatter(x, y, z, c=rgb, s=10)
+                #     # Add a subplot for the 3D scatter plot
+                #     ax = fig.add_subplot(122, projection='3d')
+                #     scat = ax.scatter(x, y, z, c=rgb, s=10)
 
-        # set ticks labels
-        ax.set_xlabel("Dimension #1")
-        ax.set_ylabel("Dimension #2")
-        ax.set_zlabel("Dimension #3")
+                #     # set ticks labels
+                #     ax.set_xlabel("Dimension #1")
+                #     ax.set_ylabel("Dimension #2")
+                #     ax.set_zlabel("Dimension #3")
 
-        # set ticks, labels to none
-        x_ticks = ax.get_xticks()
-        y_ticks = ax.get_yticks()
-        z_ticks = ax.get_zticks()
-        labels = ["" for _ in range(len(x_ticks))]
-        ax.set_xticklabels(labels)
-        ax.set_yticklabels(labels)
-        ax.set_zticklabels(labels)
+                #     # set ticks, labels to none
+                #     x_ticks = ax.get_xticks()
+                #     y_ticks = ax.get_yticks()
+                #     z_ticks = ax.get_zticks()
+                #     labels = ["" for _ in range(len(x_ticks))]
+                #     ax.set_xticklabels(labels)
+                #     ax.set_yticklabels(labels)
+                #     ax.set_zticklabels(labels)
 
-        plt.suptitle(title)
-        plt.show()
+                #     plt.suptitle(title)
+                #     plt.show()
 
-        # Define the update function for the animation
-        def update(frame):
-            if frame <= 360:
-                ax.view_init(elev=10., azim=frame)
-            if frame > 360 and frame <= 720:
-                ax.view_init(elev=frame-360, azim=10.)
+                #     # Define the update function for the animation
+                #     def update(frame):
+                #         if frame <= 360:
+                #             ax.view_init(elev=10., azim=frame)
+                #         if frame > 360 and frame <= 720:
+                #             ax.view_init(elev=frame-360, azim=10.)
 
-        # Create the animation
-        ani = FuncAnimation(fig, update, frames=np.arange(0, 360, 3), interval=60)
-        from IPython.display import HTML
-        html = HTML(ani.to_jshtml())
-        display(html)
+                #     # Create the animation
+                #     ani = FuncAnimation(fig, update, frames=np.arange(0, 360, 3), interval=60)
+                #     from IPython.display import HTML
+                #     html = HTML(ani.to_jshtml())
+                #     display(html)
 
-    # 6. 
-    def plot_3d(X_3d, rgb, title):
-        x, y, z = X_3d.T
-        fig = plt.figure(figsize=(10, 5))
+                # 6. 
+                def plot_3d(X_3d, rgb, title):
+                    x, y, z = X_3d.T
+                    fig = plt.figure(figsize=(10, 5)) # used to be (10, 5)
 
-        # Add a subplot for the static image
-        ax1 = fig.add_subplot(111)
-        ax1.imshow(rgb.reshape(h, w, 3))
-        ax1.axis('off')  # Hide axes
+                    # Add a subplot for the static image 
+                    ax1 = fig.add_subplot(111)
+                    ax1.imshow(rgb.reshape(h, w, 3))
+                    ax1.axis('off')  # Hide axes
+                    # plt.suptitle(title)
 
-        # # Add a subplot for the 3D scatter plot --> NOT USING CURRENTLY
-        # ax = fig.add_subplot(122, projection='3d')
-        # scat = ax.scatter(x, y, z, c=rgb, s=10)
+                    if(UMAP == True):
+                        plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_texture_faces/Texture_Face" + str(image_number) + "_" + str(pose) + "_Layer" + str(layer_number) + "_" + str(number_eigenvectors) + "Eigenvectors_UMAP_DINOv2.jpg")
+                    elif(tSNE == True):
+                        plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_texture_faces/Texture_Face" + str(image_number) + "_" + str(pose) + "_Layer" + str(layer_number) + "_" + str(number_eigenvectors) + "Eigenvectors_tSNE_DINOv2.jpg")
 
-        # # set ticks labels
-        # ax.set_xlabel("Dimension #1")
-        # ax.set_ylabel("Dimension #2")
-        # ax.set_zlabel("Dimension #3")
+                # 7. 
+                X_3d, rgb = rgb_from_umap_sphere(eigenvectors[:, :10], device="cpu", n_neighbors=100, min_dist=0.1)
+                plot_3d(X_3d, rgb, "Top " + str(number_eigenvectors) + " Ncut eigenvectors from DiNOv2 layer " + str(layer_number) + " features using UMAP")
 
-        # # set ticks, labels to none
-        # x_ticks = ax.get_xticks()
-        # y_ticks = ax.get_yticks()
-        # z_ticks = ax.get_zticks()
-        # labels = ["" for _ in range(len(x_ticks))]
-        # ax.set_xticklabels(labels)
-        # ax.set_yticklabels(labels)
-        # ax.set_zticklabels(labels)
-
-        # plt.suptitle(title)
-        if(UMAP == True):
-            plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfies/Selfie" + str(image_number) + "_Layer" + str(layer_number) + "_" + str(number_eigenvectors) + "Eigenvectors_UMAP_DINOv2.jpg")
-            plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfies/Selfie" + str(image_number) + "_Layer" + str(layer_number) + "_" + str(number_eigenvectors) + "Eigenvectors_UMAP_DINOv2.jpg")
-        elif(tSNE == True):
-            plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfies/Selfie" + str(image_number) + "_Layer" + str(layer_number) + "_" + str(number_eigenvectors) + "Eigenvectors_tSNE_DINOv2.jpg")
-            plt.savefig("/home/kate/Selfie2Portrait/ncut_pytorch/ncut_features_for_selfies/Selfie" + str(image_number) + "_Layer" + str(layer_number) + "_" + str(number_eigenvectors) + "Eigenvectors_tSNE_DINOv2.jpg")
-
-
-
-    # 7. 
-    X_3d, rgb = rgb_from_umap_sphere(eigenvectors[:, :10], device="cpu", n_neighbors=100, min_dist=0.1)
-    plot_3d(X_3d, rgb, "Top " + str(number_eigenvectors) + " Ncut eigenvectors from DiNOv2 layer " + str(layer_number) + " features using UMAP")
-
-    # 8.
-    UMAP = False
-    tSNE = True
-    X_3d, rgb = rgb_from_tsne_3d(eigenvectors[:, :10], device="cpu", perplexity=100)
-    plot_3d(X_3d, rgb, "Top " + str(number_eigenvectors) + " Ncut eigenvectors from DiNOv2 layer " + str(layer_number) + " features using tSNE")
-
+                # 8.
+                UMAP = False
+                tSNE = True
+                X_3d, rgb = rgb_from_tsne_3d(eigenvectors[:, :10], device="cpu", perplexity=100)
+                plot_3d(X_3d, rgb, "Top " + str(number_eigenvectors) + " Ncut eigenvectors from DiNOv2 layer " + str(layer_number) + " features using tSNE")
+            
+            print("Subprocess done...")
 
 print("Process complete!")
 
